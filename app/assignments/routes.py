2a9ab67d20e8d9509c5c3295bbe1d6cd710e8c06
udarensamolet <udarensamolet@yahoo.com>
2025-08-24 11:31:53 +0300

    fixed app.assignments import

--- app/assignments/routes.py
+++ app/assignments/routes.py
@@ -1,12 +1,11 @@
 from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import login_required, current_user
-from sqlalchemy.orm import joinedload
-
 from ..extensions import db
 from ..models.assignment import CareAssignment
 from ..models.care import CareRequest
 
 assignments_bp = Blueprint("assignments", __name__, template_folder="../templates")
+
 
 @assignments_bp.route("/assignments", methods=["GET"])
 @login_required
@@ -14,7 +13,6 @@ def list_assignments():
     owner_rows = (
         db.session.query(CareAssignment)
         .join(CareRequest, CareRequest.id == CareAssignment.care_request_id)
-        .options(joinedload(CareAssignment.__table__))
         .filter(CareRequest.owner_id == current_user.id)
         .order_by(CareAssignment.start_at.desc())
         .all()
@@ -30,6 +28,7 @@ def list_assignments():
         "assignments_list.html", owner_rows=owner_rows, sitter_rows=sitter_rows
     )
 
+
 @assignments_bp.route("/assignments/<int:assign_id>/cancel", methods=["POST"])
 @login_required
 def cancel_assignment(assign_id):
@@ -41,7 +40,6 @@ def cancel_assignment(assign_id):
     if a.status != "active":
         flash("Assignment is not active.", "info")
         return redirect(url_for("assignments.list_assignments"))
-
     a.status = "cancelled"
     db.session.commit()
     flash("Assignment cancelled.", "info")
